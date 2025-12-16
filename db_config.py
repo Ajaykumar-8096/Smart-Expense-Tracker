88b9cf4a710fa904544f3f5df710d3fd34b9100e
Ajay Kumar <ajaybalavanthapu@gmail.com>
2025-12-16 11:03:41 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Ajay@8096",
+        password="enter password",
         database="expense_tracker"
     )
